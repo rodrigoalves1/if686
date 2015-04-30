e9d0d1cd6da4ffe8fbca4b94cf8beb9a2a463a97
rodrigoalves1 <raan@cin.ufpe.br>
2015-04-30 10:03:50 -0300

    aula 30/04

---
+++ 20150430.hs
@@ -0,0 +1,12 @@
+data Failable t = Error String | Value t 
+
+instance Monad Failable where
+(>>=) (Value x) f = f x
+(>>=) (Error x) _ = (Error x)
+return x = (Value x)
+
+data Fila u = NilF | Fila (Int,u,Fila u)
+
+criarFila :: Show t => Int -> t -> Failable (t, Fila t)
+criarFila 0 _ = Error "Fila deve conter 1 elemento"
+criarFila i u = Value (u,Fila (i,u,NilF))
